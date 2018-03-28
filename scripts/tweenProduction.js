0d84ca4bd54d642bb15c5ad1d0fc6f1c85ad0293
Don_Tri <triantafillos3@gmail.com>
2018-03-28 20:49:07 +0200

    no message

---
+++ scripts/tweenProduction.js
@@ -0,0 +1,25 @@
+function tweenForEating(args) {
+    var start = args.sphere.getScale();
+    var end = args.scale;
+    console.log("start"+ start + "end" + end);
+
+    var duration = 2000;
+
+    var tween = new TWEEN.Tween(start).to(end, duration);
+
+    tween.onUpdate(function() {
+        console.log(args);
+        //console.log("start.x:" + start + "........start.y:" + start + "    ......start.z:" + start);
+        args.sphere.scaleMe({
+        scale: start
+        }); // we scale the mesh
+
+
+    });
+
+    tween.easing(TWEEN.Easing.Linear.None);
+
+    tween.start();
+
+
+}
